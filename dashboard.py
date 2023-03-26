b26711504ce8deaab4b9f69b66824a7029a2676b
praveenkkr82 <127859263+praveenkkr82@users.noreply.github.com>
2023-03-26 13:54:48 +0530

    Add new changes

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import os
-os.getcwd()
-os.chdir("C:/Users/praveen.ramaradhya/My Drive/bits-pilani/DSE/SEM4/dissertation/BNPL/sources")
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
@@ -15,45 +7,32 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 
 st.set_page_config( page_title="Dashboard", page_icon="🧊", layout="wide", initial_sidebar_state="expanded")
-st.title("​BUY NOW PAY LATER")
+st.title("​Buy Now Pay Later")
 
 
 def extract():
     # extract data from database
-    import pymongo
-    client = pymongo.MongoClient("mongodb+srv://dftML:dftML@stano.mez9zwp.mongodb.net/?retryWrites=true&w=majority")
-    database = client['staine']
-    collection = database['BNPL']
-
-    return pd.DataFrame(list(collection.find())).drop("_id", axis=1)
-
+    df = pd.read_csv('BNPL_data.csv')
+    return df
 def model_fit(df):
-
     df_dep = df.drop("Cost Matrix(Risk)", axis=1)
     df_ind = df["Cost Matrix(Risk)"]
-
     obj = [i for i in df_dep.columns if df_dep[i].dtype == "O"]
     df_dep_obj = pd.get_dummies(df[obj])
     num = [i for i in df_dep.columns if df_dep[i].dtype != "O"]
-
     df_dep = pd.concat([df_dep_obj, df[num]], axis=1)
     df_ind = np.where(df_ind=="Good Risk", 1, 0)
     df_de = pd.DataFrame()
     for i in sorted(df_dep.columns):
         df_de[i] = df_dep[i]
-
-    model = RandomForestClassifier()
+    #model = RandomForestClassifier()
+    model = pickle.load(open('randomforest_model.pkl', 'rb'))
     model.fit(df_de, df_ind)
-
     return model, df_de.iloc[1:2,:]
-
-
 if __name__=="__main__":
     df = extract()
     actual_model, df_dep = model_fit(df)
-
     df_col = [i for i in df.columns if df[i].dtypes == "O"]
-
     df_predict1 = pd.DataFrame()
     for i in range(len(df_col)-1):
         exec(f"s_{i+1} = st.selectbox(df_col[{i}], (j for j in df[df_col].iloc[:,{i}].unique()))")
@@ -61,14 +40,11 @@ if __name__=="__main__":
         df_select = pd.DataFrame(dict ([(df_col[i]+'_'+m,1 if eval(f"s_{i+1}") == m else 0 ) for m in df[df_col].iloc[:,i].unique()]), index=[0])
         df_predict1 = pd.concat([df_predict1, df_select], axis=1)
     
-
     df_int = [i for i in df.columns if df[i].dtypes != "O"]
     d2 = dict()
     for k in range(len(df_int)):
-
         exec(f"s_{k+1+len(df_col)} = st.number_input(df_int[{k}],step=1, min_value=df[df_int[{k}]].min(), max_value=df[df_int[{k}]].max(), value = df[df_int[{k}]][500])")
         exec(f"d2[df_int[{k}]] = s_{k+1+len(df_col)}")
-
     df_predict2 = pd.DataFrame(d2, index=[0])
     df_predict = pd.concat([df_predict1, df_predict2], axis=1)
     
@@ -76,9 +52,5 @@ if __name__=="__main__":
     df_pre = pd.DataFrame()
     for i in sorted(df_dep.columns):
         df_pre[i] = df_predict[i]
-
     if st.button("Predict"):
-        st.write("The customer is eligible for offer" if 1==actual_model.predict(df_pre)[0] else "The customer is not eligible for offer")
-
-
-# In[ ]:
+        st.write("Prediction: Good Risk" if 1==actual_model.predict(df_pre)[0] else "Prediction: Bad Risk")
