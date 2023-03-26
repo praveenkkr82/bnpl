ad5b3403ebef9b1465d65bffe90d7beca50efa1e
praveenkkr82 <127859263+praveenkkr82@users.noreply.github.com>
2023-03-26 10:45:43 +0530

    Changed Title

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 
 st.set_page_config( page_title="Dashboard", page_icon="🧊", layout="wide", initial_sidebar_state="expanded")
-st.title("​Buy Now Pay Later")
+st.title("​Watch Now Pay Later")
 
 
 def extract():
